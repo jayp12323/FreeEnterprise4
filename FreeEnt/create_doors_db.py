f3776afa64bdd52eff6e2b988a004ab54b6fa2cf
jayp12323 <jayp12323@gmail.com>
2024-07-25 08:33:03 -0600

    adding tororia trigger and reformatting

--- FreeEnt/create_doors_db.py
+++ FreeEnt/create_doors_db.py
@@ -14,12 +14,12 @@ overworld = ['#AdamantGrotto', '#Agart', '#AgartArmor', '#AgartInn', '#AgartWeap
              '#Silvera', '#SilveraArmor', '#SilveraInn', '#SilveraItems', '#SilveraWeapons', '#ToroiaArmor',
              '#ToroiaCafe', '#ToroiaCastle', '#ToroiaInn', '#ToroiaItem', '#ToroiaStable', '#ToroiaTown',
              '#ToroiaWeapon', '#TrainingRoomMain', '#TroiaChocoboForest', '#Waterfall2F', '#WaterfallEntrance',
-             '#WateryPass1F', '#WateryPass5F', "#Overworld",'#SoldierAirship']
+             '#WateryPass1F', '#WateryPass5F', "#Overworld", '#SoldierAirship']
 underworld = ['#Babil1F', '#CaveOfSummons1F', '#DwarfCastle', '#DwarfCastleBasement', '#SealedCaveEntrance',
               '#SmithyHouse', '#SylvanCave1F', '#Tomra', '#TomraEquipment', '#TomraInn', '#TomraInn', '#TomraItem',
-              '#TomraTreasury',"#Underworld"]
+              '#TomraTreasury', "#Underworld"]
 
-moon = ['#Bahamut1F', '#Hummingway', '#LunarPalaceLobby', '#LunarPassage1', '#LunarPassage2',"#Moon"]
+moon = ['#Bahamut1F', '#Hummingway', '#LunarPalaceLobby', '#LunarPassage1', '#LunarPassage2', "#Moon"]
 
 map_key = {}
 for i in overworld:
@@ -61,26 +61,30 @@ for line in lines:
                 else:
                     door_type = "interior"
                 if door_type != "interior":
-                    if target_loc in ["#Overworld", "#Underworld", "#Moon"] and loc in ["#LunarPassage1","#LunarPassage2","#MistCave","#Mist"]:
-                        if number in ["4","1"]:
-                            facing="up"
-                        elif number=="7":
-                            facing="right"
-                        elif number=="8":
-                            facing="left"
+                    if target_loc in ["#Overworld", "#Underworld", "#Moon"] and loc in ["#LunarPassage1",
+                                                                                        "#LunarPassage2", "#MistCave",
+                                                                                        "#Mist"]:
+                        if number in ["4", "1"]:
+                            facing = "up"
+                        elif number == "7":
+                            facing = "right"
+                        elif number == "8":
+                            facing = "left"
                         else:
-                            facing="down"
+                            facing = "down"
                     triggers.append(
-                        [loc, number, x, y, target_loc, target_x, target_y, facing, door_type, f"{loc}_{target_loc}_{facing}",
+                        [loc, number, x, y, target_loc, target_x, target_y, facing, door_type,
+                         f"{loc}_{target_loc}_{facing}",
                          map_key[target_loc]])
         trigger = [line]
         start = 1
     else:
         trigger.append(line)
 DB_PATH = os.path.join(os.path.dirname(__file__), 'assets', 'db')
-COLUMNS = ['map', 'trigger_number', 'x', 'y', 'dest', 'dest_x', 'dest_y', 'facing', 'type', "name","world"]
+COLUMNS = ['map', 'trigger_number', 'x', 'y', 'dest', 'dest_x', 'dest_y', 'facing', 'type', "name", "world"]
 
-to_remove = ["#SoldierAirship", "#GiantMouth","#MysidiaSerpentRoad",'#BlackChocoboForest',"#SealedCaveEntrance",'#AdamantGrotto','#CaveEblanEntrance']
+to_remove = ["#SoldierAirship", "#GiantMouth", "#MysidiaSerpentRoad", '#BlackChocoboForest', "#SealedCaveEntrance",
+             '#AdamantGrotto', '#CaveEblanEntrance']
 
 for map_to_remove in to_remove:
     for trigger in triggers:
@@ -89,12 +93,16 @@ for map_to_remove in to_remove:
             break
 
 hardcoded = [["#Underworld", "5", "48", "15", "#Babil1F", "15", "24", "up", "entrance", "#Underworld_#Babil1F_up",
-             "#Underworld"],["#Underworld", "6", "49", "15", "#Babil1F", "15", "24", "up", "entrance", "#Underworld_#Babil1F_up",
-             "#Underworld"],
-             ["#Silvera", "7", "17", "31", "#Overworld", "210", "130", "", "exit","#Silvera_#Overworld_","#Overworld"]
+              "#Underworld"],
+             ["#Underworld", "6", "49", "15", "#Babil1F", "15", "24", "up", "entrance", "#Underworld_#Babil1F_up",
+              "#Underworld"],
+             ["#Silvera", "7", "17", "31", "#Overworld", "210", "130", "", "exit", "#Silvera_#Overworld_",
+              "#Overworld"],
+             ["#ToroiaTown", "11", "16", "29", "#Overworld", "36", "83", "", "exit", "#ToroiaTown_#Overworld_",
+              "#Overworld"]
              ]
 
-triggers+=hardcoded
+triggers += hardcoded
 with open(os.path.join(DB_PATH, 'doors.csvdb'), 'w', newline='') as file:
     csv_file = csv.writer(file)
     csv_file.writerow(COLUMNS)
