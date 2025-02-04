3f4593e89b7c7e1646da761edfa28170def2b6d0
jayp1232@gmail.com <jayp1232@gmail.com>
2025-02-04 14:26:10 -0600

    Finished creating floors db

--- FreeEnt/create_floors_db.py
+++ FreeEnt/create_floors_db.py
@@ -1,4 +1,5 @@
 import collections
+import csv
 import json
 import os
 
@@ -242,7 +243,7 @@ for map in warps_to_map:
             # map, trigger_number, x, y, dest, dest_x, dest_y, facing, type, name, world
 
             mapgrid_to_replace.append("mapgrid ({} {} {}) {{ {} }}".format(master_map["maps"][map]["mapgrid_id"], warp_trigger[0],warp_trigger[1],tile_id ))
-            db.append([map,last_trigger,warp_trigger[0],warp_trigger[1],dest,x,y,"up","exit","{}_{}_{}".format(map,dest,"up"),map_key[map]])
+            db.append([map,last_trigger,warp_trigger[0],warp_trigger[1],dest,x,y,"up","exit","{}_{}_{}_{}".format(map,dest,"up",last_trigger),map_key[map]])
 
             # print(map, warp_trigger, warps_to_map[map]["warp_to_trigger"][warp_trigger])
 for m in mapgrid_to_replace:
@@ -250,116 +251,105 @@ for m in mapgrid_to_replace:
 
 for d in db:
     print(d)
-overworld = ["#CaveEblanEquipment", "#CaveEblanInn", "#FabulInn", "#Overworld", "#ToroiaCastleHospital",
-             "#ToroiaCastleStairs", '#AdamantGrotto', '#Agart', '#AgartArmor', '#AgartInn', '#AgartWeapon',
-             '#AgartWell', '#AntlionCave1F', '#AstroTower', '#BabilB1', '#BaronCastle', '#BaronCastleEastTower1F',
-             '#BaronCastleEastTower2F', '#BaronCastleEastTowerB1', '#BaronCastlePrisonEntrance',
-             '#BaronCastleSoldiersQuarters', '#BaronChocoboForest', '#BaronEquipment', '#BaronInn', '#BaronSerpentRoad',
-             '#BaronTown', '#BaronTownItems', '#BlackChocoboForest', '#CaveEblanEntrance', '#CaveEblanExit',
-             '#CaveMagnes1F', '#CaveMagnes2F', '#CaveMagnes3F', '#CaveMagnes4F', '#CaveMagnes5F', '#CidHouse',
-             '#Damcyan', '#Damcyan1F', '#Damcyan2F', '#DamcyanTreasuryEntrance', '#Eblan', '#Eblan1F', '#Eblan2F',
-             '#EblanEastTower1F', '#EblanEastTower2F', '#EblanThroneRoom', '#EblanWestTower1F', '#EblanWestTower2F',
-             '#Fabul', '#FabulChocoboForest', '#FabulEquipment', '#FabulHospital', '#FabulWestTower1F', '#GiantMouth',
-             '#HouseOfWishes', '#IslandChocoboForest', '#Kaipo', '#KaipoArmor', '#KaipoHospital', '#KaipoInn',
-             '#KaipoWeapon', '#Mist', '#MistArmor', '#MistCave', '#MistInn', '#MistWeapon', '#MountHobsEast',
-             '#MountHobsWest', '#MountOrdeals1F', '#MountOrdealsChocoboForest', '#Mysidia', '#MysidiaArmor',
-             '#MysidiaCafe', '#MysidiaItem', '#MysidiaSerpentRoad', '#MysidiaWeapon', '#RoomToSewer', '#RosaHouse',
-             '#RydiaHouse', '#Silvera', '#SilveraArmor', '#SilveraInn', '#SilveraItems', '#SilveraWeapons',
-             '#SoldierAirship', '#ToroiaArmor', '#ToroiaCafe', '#ToroiaCastle', '#ToroiaCastleHall',
-             '#ToroiaCastleStairs', '#ToroiaInn', '#ToroiaItem', '#ToroiaStable', '#ToroiaTown', '#ToroiaWeapon',
-             '#TrainingRoomMain', '#TrainingRoomMain', '#TroiaChocoboForest', '#Waterfall2F', '#WaterfallEntrance',
-             '#WateryPass1F', '#WateryPass5F', '#Zot2F', '#Zot3F', '#Zot4F', '#Zot5F', '#Zot6F']
-underworld = ["#Feymarch1F", "#Feymarch2F", "#FeymarchLibrary1F", "#FeymarchSaveRoom", "#FeymarchTreasury",
-              "#FeymarchWeapon", "#Underworld", '#Babil1F', '#Babil4F', '#BabilFloorAirship2', '#BabilFloorIceMail',
-              '#BabilFloorIceMail2', '#CaveOfSummons1F', '#CaveOfSummons2F', '#CaveOfSummons3F', '#DwarfCastle',
-              '#DwarfCastleBasement', '#DwarfCastleCrystalRoom', '#DwarfCastleTower2F', '#Feymarch1F', '#Feymarch2F',
-              '#FeymarchArmor', '#FeymarchInn', '#FeymarchLibrary1F', '#FeymarchLibrary2F', '#FeymarchTreasury',
-              '#SealedCave1F', '#SealedCave2F', '#SealedCave3F', '#SealedCave4F', '#SealedCave5F', '#SealedCave6F',
-              '#SealedCave7F', '#SealedCaveCrystalRoom', '#SealedCaveDemonWallRoom', '#SealedCaveEntrance',
-              '#SealedCaveSaveRoom', '#SmithyHouse', '#SmithyHouseMainFloor', '#SylvanCave1F', '#SylvanCave2F',
-              '#SylvanCave3F', '#SylvanCaveYangRoom', '#Tomra', '#TomraEquipment', '#TomraInn', '#TomraItem',
-              '#TomraTreasury']
 
-moon = ['#Bahamut1F', '#Hummingway', '#LunarPalaceLobby', '#LunarPassage1', '#LunarPassage2', "#Moon", '#Bahamut2F',
-        '#LunarSubterran1F', '#LunarSubterran2F', '#LunarSubterran3F', '#LunarSubterran4F', '#LunarSubterran5F',
-        '#LunarSubterran6F', '#LunarSubterran7F', '#LunarCore1F', '#LunarCore2F', '#LunarCore3F', '#LunarCore4F',
-        '#LunarSubterranTunnelCure3', '#LunarSubterranTunnelProtectRing', '#LunarSubterranTunnelWhiteRobe',
-        '#LunarSubterranSaveRoom1', '#LunarSubterranTunnelMinerva']
 
-map_key = {}
-for i in overworld:
-    map_key[i] = "#Overworld"
-for i in underworld:
-    map_key[i] = "#Underworld"
-for i in moon:
-    map_key[i] = "#Moon"
+lines=list(triggers)
 
-# trigger = ""
-# start = 0
-# triggers = []
-# towns = ['#BaronTown', '#Feymarch1F', '#Feymarch2F', '#Mist', '#Kaipo', '#Mysidia', '#Silvera', '#ToroiaTown', '#Agart',
-#          '#Tomra']
-# for line in triggers:
-#     line = line.strip()
-#     print(line)
-#     if "trigger" in line:
-#         if start == 1:
-#             if "teleport" in trigger[3]:
-#                 loc, number = trigger[0].split("#")[1].strip(")").split(" ")
-#                 loc = "#" + loc
-#                 x, y = trigger[2].split(" ")[1:]
-#                 target = trigger[3].split(" ")
-#                 target_loc = target[1]
-#                 target_x = target[3]
-#                 target_y = target[4]
-#                 try:
-#                     facing = target[6]
-#                 except IndexError:
-#                     facing = ""
-#                 if target_loc in ["#Overworld", "#Underworld", "#Moon"]:
-#                     door_type = "exit"
-#                 elif loc in ["#Overworld", "#Underworld", "#Moon"]:
-#                     door_type = "entrance"
-#                 elif loc in towns:
-#                     door_type = "town_building"
-#                 elif target_loc in towns:
-#                     if loc in ["#CaveOfSummons3F"]:
-#                         door_type = "town_building"
-#                     else:
-#                         door_type = "return"
-#                 elif target_loc == '#BabilB1' and loc == '#CaveEblanExit':
-#                     door_type = "town_building"
-#
-#                 elif target_loc == '#CaveEblanExit' and loc == '#BabilB1':
-#                     door_type = "exit"
-#                 elif loc == "#SylvanCaveYangRoom":
-#                     door_type = "return"
-#                 elif target_loc in ["#SylvanCaveYangRoom","#FabulInn",'#FabulEquipment','#FabulWestTower1F',
-#                                     "#ToroiaCastleHospital","#ToroiaCastleStairs","#CaveEblanEquipment","#CaveEblanInn"]:
-#                     door_type = "town_building"
-#                 else:
-#                     print([loc, number, x, y, target_loc, target_x, target_y,])
-#
-#                     door_type = "interior"
-#                 if door_type != "interior":
-#                     if target_loc in ["#Overworld", "#Underworld", "#Moon"] and loc in ["#LunarPassage1",
-#                                                                                         "#LunarPassage2", "#MistCave",
-#                                                                                         "#Mist"]:
-#                         if number in ["4", "1"]:
-#                             facing = "up"
-#                         elif number == "7":
-#                             facing = "right"
-#                         elif number == "8":
-#                             facing = "left"
-#                         else:
-#                             facing = "down"
-#                     triggers.append(
-#                         [loc, number, x, y, target_loc, target_x, target_y, facing, door_type,
-#                          f"{loc}_{target_loc}_{facing}",
-#                          map_key[target_loc]])
-#         trigger = [line]
-#         start = 1
-#     else:
-#         trigger.append(line)
-# DB_PATH = os.path.join(os.path.dirname(__file__), 'assets', 'db')
-# COLUMNS = ['map', 'trigger_number', 'x', 'y', 'dest', 'dest_x', 'dest_y', 'facing', 'type', "name", "world"]
+trigger = ""
+start = 0
+triggers = []
+towns = ['#BaronTown', '#Feymarch1F', '#Feymarch2F', '#Mist', '#Kaipo', '#Mysidia', '#Silvera', '#ToroiaTown', '#Agart',
+         '#Tomra']
+for line in lines:
+    line = line.strip()
+    if "trigger" in line:
+        if start == 1:
+            if "teleport" in trigger[3]:
+                loc, number = trigger[0].split("#")[1].strip(")").split(" ")
+                loc = "#" + loc
+                x, y = trigger[2].split(" ")[1:]
+                target = trigger[3].split(" ")
+                target_loc = target[1]
+                target_x = target[3]
+                target_y = target[4]
+                try:
+                    facing = target[6]
+                except IndexError:
+                    facing = ""
+                if target_loc in ["#Overworld", "#Underworld", "#Moon"]:
+                    door_type = "exit"
+                elif loc in ["#Overworld", "#Underworld", "#Moon"]:
+                    door_type = "entrance"
+                elif loc in towns:
+                    door_type = "town_building"
+                elif target_loc in towns:
+                    if loc in ["#CaveOfSummons3F"]:
+                        door_type = "town_building"
+                    else:
+                        door_type = "return"
+                elif target_loc == '#BabilB1' and loc == '#CaveEblanExit':
+                    door_type = "town_building"
+
+                elif target_loc == '#CaveEblanExit' and loc == '#BabilB1':
+                    door_type = "exit"
+                elif loc == "#SylvanCaveYangRoom":
+                    door_type = "return"
+                elif target_loc in ["#SylvanCaveYangRoom","#FabulInn",'#FabulEquipment','#FabulWestTower1F',
+                                    "#ToroiaCastleHospital","#ToroiaCastleStairs","#CaveEblanEquipment","#CaveEblanInn"]:
+                    door_type = "town_building"
+                else:
+                    print([loc, number, x, y, target_loc, target_x, target_y,])
+
+                    door_type = "interior"
+                if door_type != "interior":
+                    if target_loc in ["#Overworld", "#Underworld", "#Moon"] and loc in ["#LunarPassage1",
+                                                                                        "#LunarPassage2", "#MistCave",
+                                                                                        "#Mist"]:
+                        if number in ["4", "1"]:
+                            facing = "up"
+                        elif number == "7":
+                            facing = "right"
+                        elif number == "8":
+                            facing = "left"
+                        else:
+                            facing = "down"
+                    if not facing:
+                        facing="up"
+                    triggers.append(
+                        [loc, number, x, y, target_loc, target_x, target_y, facing, door_type,
+                         f"{loc}_{target_loc}_{facing}_{number}",
+                         map_key[target_loc]])
+        trigger = [line]
+        start = 1
+    else:
+        trigger.append(line)
+DB_PATH = os.path.join(os.path.dirname(__file__), 'assets', 'db')
+COLUMNS = ['map', 'trigger_number', 'x', 'y', 'dest', 'dest_x', 'dest_y', 'facing', 'type', "name", "world"]
+
+to_remove = ["#SoldierAirship", "#GiantMouth", "#MysidiaSerpentRoad", "#BaronSerpentRoad",
+             "#TrainingRoomMain"]
+
+for map_to_remove in to_remove:
+    for trigger in list(triggers):
+        if map_to_remove == trigger[4] :
+            triggers.remove(trigger)
+        elif map_to_remove == trigger[0]:
+            triggers.remove(trigger)
+        elif "#SylvanCaveYangRoom" == trigger[4] and "#SylvanCaveYangRoom" == trigger[0]:
+            triggers.remove(trigger)
+        # elif "#Underworld" == trigger[4] and "#SylvanCaveYangRoom" == trigger[0]:
+        #     triggers.remove(trigger)
+
+hardcoded = [["#Underworld", "5", "48", "15", "#Babil1F", "15", "24", "up", "entrance", "#Underworld_#Babil1F_up",
+              "#Underworld"],
+             ["#Underworld", "6", "49", "15", "#Babil1F", "15", "24", "up", "entrance", "#Underworld_#Babil1F_up",
+              "#Underworld"],
+             ]
+
+
+triggers += hardcoded
+triggers += db
+with open(os.path.join(DB_PATH, 'floors.csvdb'), 'w', newline='') as file:
+    csv_file = csv.writer(file)
+    csv_file.writerow(COLUMNS)
+    csv_file.writerows(triggers)
